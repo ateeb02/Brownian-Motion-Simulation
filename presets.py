901313620abca75dac19aeab9e2564e0b31fe362
Ateeb Tahir <ateeb.tahir.16@gmail.com>
2023-07-03 00:11:44 +0500

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,1 +1,10 @@
 # Change The Simulation Presets Here
+
+# Simulation parameters
+num_particles     = 200
+container_width   = 400
+container_height  = 300
+particle_radius   = 3
+particle_mass     = 1.0
+elasticity        = 0.8
+simulation_speed  = 0.1
